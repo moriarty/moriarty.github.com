ce1aded44b8cab4f7cc02349ff0bc85bc260ff0d
Alexander Moriarty <alexander@dal.ca>
2012-11-27 02:45:08 -0400

    more work on final project

--- js/final.js
+++ js/final.js
@@ -15,8 +15,11 @@ function UpdateTableHeaders() {
            scrollTop      = $(window).scrollTop(),
            floatingHeader = $(".floatingHeader", this),
            width = $(this).css("width") // padding is 20 on each side
+           mleft = $("#container").css("margin-left")
+           mright = $("#container").css("margin-right")
        floatingHeader.css({
-          "width": width
+          "width": width,
+          "opacity": "1"
        })
        if ((scrollTop > offset.top -31) && (scrollTop < offset.top -31 + el.height())) {
            floatingHeader.css({
@@ -32,7 +35,7 @@ function UpdateTableHeaders() {
 
 // DOM Ready      
 $(function() {
-
+   $("#topnavlist").tinyNav();
    var clonedHeaderRow;
 
    $(".persist-area").each(function() {
